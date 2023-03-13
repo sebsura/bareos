aa95c6ef389886ea6537d529288e654d32c48ec4
Sebastian Sura <sebastian.sura@bareos.com>
2023-03-13 12:31:53 +0100

    bfile: use kInvalidFiledescriptor
    We should not assign a valid file descriptor to a bfd that was not
    initialized yet.
    Because of this IsBopen(bfd) curretly returns true (on unix systems)
    if bfd was a newly created fd on which binit() was not called.
    On windows systems it already correctly returns false in that
    situation.  Now it always returns false.
    
    Also uses kInvalidFiledescriptor when checking for invalid fds to stay
    consistent.

--- core/src/findlib/bfile.cc
+++ core/src/findlib/bfile.cc
@@ -367,7 +367,7 @@ extern "C" HANDLE get_osfhandle(int fd);
 void binit(BareosFilePacket* bfd)
 {
   new (bfd) BareosFilePacket();
-  bfd->filedes = -1;
+  bfd->filedes = kInvalidFiledescriptor;
   bfd->use_backup_api = have_win32_api();
 }
 
@@ -1039,19 +1039,19 @@ int bopen(BareosFilePacket* bfd,
   bfd->filedes = open(fname, flags & ~O_NOATIME, mode);
 
   /* Set O_NOATIME if possible */
-  if (bfd->filedes != -1 && flags & O_NOATIME) {
+  if (bfd->filedes != kInvalidFiledescriptor && flags & O_NOATIME) {
     int oldflags = fcntl(bfd->filedes, F_GETFL, 0);
     if (oldflags == -1) {
       bfd->BErrNo = errno;
       close(bfd->filedes);
-      bfd->filedes = -1;
+      bfd->filedes = kInvalidFiledescriptor;
     } else {
       int ret = fcntl(bfd->filedes, F_SETFL, oldflags | O_NOATIME);
       /* EPERM means setting O_NOATIME was not allowed  */
       if (ret == -1 && errno != EPERM) {
         bfd->BErrNo = errno;
         close(bfd->filedes);
-        bfd->filedes = -1;
+        bfd->filedes = kInvalidFiledescriptor;
       }
     }
   }
@@ -1065,7 +1065,7 @@ int bopen(BareosFilePacket* bfd,
 
 #  if defined(HAVE_POSIX_FADVISE) && defined(POSIX_FADV_WILLNEED)
   /* If not RDWR or WRONLY must be Read Only */
-  if (bfd->filedes != -1 && !(flags & (O_RDWR | O_WRONLY))) {
+  if (bfd->filedes != kInvalidFiledescriptor && !(flags & (O_RDWR | O_WRONLY))) {
     int status = posix_fadvise(bfd->filedes, 0, 0, POSIX_FADV_WILLNEED);
     Dmsg3(400, "Did posix_fadvise WILLNEED on %s filedes=%d status=%d\n", fname,
           bfd->filedes, status);
@@ -1097,13 +1097,13 @@ int bclose(BareosFilePacket* bfd)
 {
   int status;
 
-  if (bfd->filedes == -1) { return 0; }
+  if (bfd->filedes == kInvalidFiledescriptor) { return 0; }
 
   Dmsg1(400, "Close file %d\n", bfd->filedes);
 
   if (bfd->cmd_plugin && plugin_bclose) {
     status = plugin_bclose(bfd);
-    bfd->filedes = -1;
+    bfd->filedes = kInvalidFiledescriptor;
     bfd->do_io_in_core = false;
     bfd->cmd_plugin = false;
   } else {
@@ -1119,7 +1119,7 @@ int bclose(BareosFilePacket* bfd)
     /* Close normal file */
     status = close(bfd->filedes);
     bfd->BErrNo = errno;
-    bfd->filedes = -1;
+    bfd->filedes = kInvalidFiledescriptor;
     bfd->cmd_plugin = false;
     bfd->do_io_in_core = false;
   }
