bf47ff9320f99161394c9a7a110de8b3f2a36046
Sebastian Sura <sebastian.sura@bareos.com>
2023-07-31 09:03:39 +0200

    dedupe: add json output

--- core/src/tools/dedupe.cc
+++ core/src/tools/dedupe.cc
@@ -29,8 +29,9 @@
 
 #include <unordered_map>
 #include <vector>
-
 #include <cassert>
+
+#include <jansson.h>
 
 template <typename F> void for_each_record(dedup::volume& vol, F callback)
 {
@@ -100,17 +101,37 @@ struct sha_aggregator {
     };
   };
 
-  using record_set = std::vector<record_id>;
+  struct record_set {
+    std::vector<std::byte> datarecord;
+    std::vector<record_id> recids{};
+
+    record_set(const std::vector<std::byte>& data) : datarecord{data} {}
+
+    std::vector<record_id>& ids() { return recids; }
+
+    const std::vector<record_id>& ids() const { return recids; }
+
+    const std::vector<std::byte>& data() const { return datarecord; }
+  };
+
   template <typename Val>
   using sha_map = std::unordered_map<sha, Val, sha::hash>;
 
-  void add_record(record_id idx, const std::vector<std::byte>& datarecord)
+  void add_record(record_id id, const std::vector<std::byte>& datarecord)
   {
     auto size = datarecord.size();
 
     auto val = sha(datarecord);
 
-    records_by_size[size][val].push_back(idx);
+    auto& map = records_by_size[size];
+
+    if (auto found = map.find(val); found != map.end()) {
+      found->second.ids().push_back(id);
+    } else {
+      auto [iter, inserted] = map.emplace(val, datarecord);
+      ASSERT(inserted);
+      iter->second.ids().push_back(id);
+    }
   }
 
   std::unordered_map<std::size_t, sha_map<record_set>> records_by_size;
@@ -125,6 +146,16 @@ struct sha_aggregator {
     return data;
   }
 };
+
+json_t* vec_as_json(const std::vector<std::size_t> vec)
+{
+  json_t* array = json_array();
+  for (auto num : vec) {
+    json_t* val = json_integer(num);
+    json_array_append_new(array, val);
+  }
+  return array;
+}
 
 int main(int argc, const char** argv)
 {
@@ -146,32 +177,88 @@ int main(int argc, const char** argv)
     dedup::volume vol{volume.c_str(), storagedaemon::DeviceMode::OPEN_READ_ONLY,
                       0, 0};
 
-    if (!vol.is_ok()) {
-      std::cout << volume << " NOT ok\n";
-      continue;
-    }
-    std::cout << volume << " ok\n";
-
+    if (!vol.is_ok()) { continue; }
     for_each_record(vol, [volidx, &agg](auto recidx, auto& data) {
       agg.add_record(record_id{volidx, recidx}, data);
     });
   }
+
   auto data = agg.data();
 
-  std::size_t singles{};
-  std::size_t lovers{};
+  std::vector<std::byte> output;
+  struct value {
+    std::size_t start{}, size{};
+    std::vector<std::size_t> records{};
+    value() = default;
+    value(std::size_t start, std::size_t size) : start{start}, size{size} {}
+  };
 
+  std::vector<std::vector<value>> vols;
+  vols.resize(volumes.size());
+  std::vector<value*> current;
+  current.resize(volumes.size());
   for (auto& set : data) {
-    std::cout << "[\n";
-    if (set.size() == 1)
-      singles += 1;
-    else
-      lovers += set.size();
-    for (auto id : set) {
-      std::cout << "  <" << id.volidx << ", " << id.recidx << ">,\n";
+    auto& datarecord = set.data();
+    auto start = output.size();
+    auto size = datarecord.size();
+
+    output.insert(output.end(), datarecord.begin(), datarecord.end());
+
+    for (auto id : set.ids()) {
+      if (current[id.volidx] == nullptr) {
+        current[id.volidx] = &vols[id.volidx].emplace_back(start, size);
+      }
+      current[id.volidx]->records.push_back(id.recidx);
     }
-    std::cout << "]\n";
+
+    current.resize(0);
+    current.resize(volumes.size());
   }
 
-  std::cout << "singles: " << singles << ", not: " << lovers << "\n";
+  json_error_t ec;
+  json_t* json_volumes = json_object();
+  for (std::size_t i = 0; i < vols.size(); ++i) {
+    auto& vol = vols[i];
+    json_t* array = json_array();
+    for (auto& val : vol) {
+      auto* ids = vec_as_json(val.records);
+      auto* json = json_pack_ex(&ec, 0, "{s:i,s:i,s:o*}", "start", val.start,
+                                "size", val.size, "records", ids);
+      if (!json) {
+        fprintf(stderr, "json error %s:%d,%d: %s\n", ec.source, ec.line,
+                ec.column, ec.text);
+        return 1;
+      }
+
+      json_array_append_new(array, json);
+    }
+    json_object_set_new(json_volumes, volumes[i].c_str(), array);
+  }
+
+  json_t* root = json_pack_ex(&ec, 0, "{s:s,s:o}", "output", outfile.c_str(),
+                              "volumes", json_volumes);
+  if (!root) {
+    fprintf(stderr, "json error %s:%d,%d: %s\n", ec.source, ec.line, ec.column,
+            ec.text);
+    return 1;
+  }
+
+
+  int outfd = open(outfile.c_str(), O_CREAT | O_APPEND | O_WRONLY);
+
+  if (outfd < 0) {
+    perror(nullptr);
+    return 1;
+  }
+
+  if (write(outfd, output.data(), output.size())
+      != static_cast<ssize_t>(output.size())) {
+    perror("bad write");
+    return 1;
+  }
+  close(outfd);
+
+  if (json_dump_file(root, replacement.c_str(), JSON_COMPACT) < 0) {
+    fprintf(stderr, "could not dump json into %s\n", replacement.c_str());
+  }
 }
