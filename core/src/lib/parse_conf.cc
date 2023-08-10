364f48f64db0f4ebafeb52a38fc47d744f1ad86a
Alaa Eddine Elamri <alaeddine.elamri@bareos.com>
2023-08-10 13:53:36 +0200

    add ParseconfigOrExit function

--- core/src/lib/parse_conf.cc
+++ core/src/lib/parse_conf.cc
@@ -67,6 +67,8 @@
 #include "lib/berrno.h"
 #include "lib/util.h"
 
+#include <iostream>
+
 
 #if defined(HAVE_WIN32)
 #  include "shlobj.h"
@@ -151,6 +153,13 @@ std::string ConfigurationParser::CreateOwnQualifiedNameForNetworkDump() const
     }
   }
   return qualified_name;
+}
+void ConfigurationParser::ParseConfigOrExit()
+{
+  if (!ParseConfig()) {
+    std::cerr << "Configuration parsing error" << std::endl;
+    exit(configerror_exit_code);
+  }
 }
 
 bool ConfigurationParser::ParseConfig()
