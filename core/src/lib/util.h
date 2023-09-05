f0130e83bfa6b91b244398157d151628b4a331b0
Sebastian Sura <sebastian.sura@bareos.com>
2023-09-05 10:12:35 +0200

    util: move result<T> into util

--- core/src/lib/util.h
+++ core/src/lib/util.h
@@ -23,6 +23,7 @@
 
 #include <sys/stat.h>
 #include <chrono>
+#include <variant>
 
 #if defined(HAVE_WIN32)
 #  include "bregex.h"
@@ -88,5 +89,48 @@ std::string CreateDelimitedStringForSqlQueries(
 std::string TPAsString(const std::chrono::system_clock::time_point& tp);
 regex_t* StringToRegex(const char* input);
 void to_lower(std::string& s);
+
+// fixme(ssura): replace by std::expected<T, PoolMem> (C++23)
+//               or std::expected<T, std::string>
+template <typename T> class result {
+ public:
+  constexpr result() : data{1, "Not Initialized"} {}
+
+  template <typename Arg0, typename... Args>
+  constexpr result(Arg0 arg0, Args... args)
+      : data(std::forward<Arg0>(arg0), std::forward<Args>(args)...)
+  {
+  }
+
+  bool holds_error() const { return data.index() == 1; }
+  PoolMem* error()
+  {
+    if (holds_error()) {
+      return &std::get<1>(data);
+    } else {
+      return nullptr;
+    }
+  }
+
+  T* value()
+  {
+    if (!holds_error()) {
+      return &std::get<0>(data);
+    } else {
+      return nullptr;
+    }
+  }
+
+  PoolMem& error_unchecked() { return std::get<1>(data); }
+
+  T& value_unchecked() { return std::get<0>(data); }
+
+ private:
+  // we are using PoolMem here, since it is easier to format into a PoolMem.
+  // Once we have access to std::format everywhere, this should change
+  // to std::string.
+  std::variant<T, PoolMem> data;
+};
+
 
 #endif  // BAREOS_LIB_UTIL_H_
