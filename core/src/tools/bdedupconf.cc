561e5596c6f3720e1401fdec8d206d1857b1f016
Sebastian Sura <sebastian.sura@bareos.com>
2024-01-16 13:53:16 +0100

    bdedup-conf: add better error messages

--- core/src/tools/bdedupconf.cc
+++ core/src/tools/bdedupconf.cc
@@ -44,6 +44,8 @@ constexpr const char* srdonly = "read_only";
 constexpr const char* sstart = "start";
 constexpr const char* send = "end";
 
+constexpr auto debug_options = JSON_INDENT(2);
+
 std::vector<char> ReadInput()
 {
   return std::vector<char>{std::istreambuf_iterator<char>(std::cin),
@@ -52,14 +54,15 @@ std::vector<char> ReadInput()
 
 // WRITING
 
-json_t* LoadJson(const char* str)
+json_t* ParseJson(const char* str)
 {
   json_error_t error;
   auto* json = json_loads(str, 0, &error);
 
   if (!json) {
-    std::cout << "error on line " << error.line << ". ERR=" << error.text
-              << std::endl;
+    throw std::runtime_error("Could not parse string to json: "
+                             + std::string{error.text} + " (line "
+                             + std::to_string(error.line) + ").");
   }
 
   return json;
@@ -70,7 +73,11 @@ template <typename T> T LoadJson(json_t* val);
 template <>
 dedup::config::block_file LoadJson<dedup::config::block_file>(json_t* obj)
 {
-  if (!json_is_object(obj)) { exit(1); }
+  if (!json_is_object(obj)) {
+    throw std::runtime_error("Expected json object, got "
+                             + std::string{json_dumps(obj, debug_options)});
+  }
+
   auto* path = json_object_get(obj, spath);
   auto* start = json_object_get(obj, sstart);
   auto* end = json_object_get(obj, send);
@@ -78,8 +85,15 @@ dedup::config::block_file LoadJson<dedup::config::block_file>(json_t* obj)
 
   if (!path || !json_is_string(path) || !start || !json_is_integer(start)
       || !end || !json_is_integer(end) || !idx || !json_is_integer(idx)) {
-    exit(1);
+    throw std::runtime_error("Could not parse block_file from "
+                             + std::string{json_dumps(obj, debug_options)});
   }
+
+  if (json_object_size(obj) > 4) {
+    throw std::runtime_error("Too many keys for block_file: "
+                             + std::string{json_dumps(obj, debug_options)});
+  }
+
 
   dedup::config::block_file bf{
       .relpath = json_string_value(path),
@@ -94,7 +108,11 @@ dedup::config::block_file LoadJson<dedup::config::block_file>(json_t* obj)
 template <>
 dedup::config::part_file LoadJson<dedup::config::part_file>(json_t* obj)
 {
-  if (!json_is_object(obj)) { exit(1); }
+  if (!json_is_object(obj)) {
+    throw std::runtime_error("Expected json object, got "
+                             + std::string{json_dumps(obj, debug_options)});
+  }
+
   auto* path = json_object_get(obj, spath);
   auto* start = json_object_get(obj, sstart);
   auto* end = json_object_get(obj, send);
@@ -102,7 +120,13 @@ dedup::config::part_file LoadJson<dedup::config::part_file>(json_t* obj)
 
   if (!path || !json_is_string(path) || !start || !json_is_integer(start)
       || !end || !json_is_integer(end) || !idx || !json_is_integer(idx)) {
-    exit(1);
+    throw std::runtime_error("Could not parse record_file from "
+                             + std::string{json_dumps(obj, debug_options)});
+  }
+
+  if (json_object_size(obj) > 4) {
+    throw std::runtime_error("Too many keys for record_file: "
+                             + std::string{json_dumps(obj, debug_options)});
   }
 
   dedup::config::part_file pf{
@@ -118,7 +142,10 @@ dedup::config::part_file LoadJson<dedup::config::part_file>(json_t* obj)
 template <>
 dedup::config::data_file LoadJson<dedup::config::data_file>(json_t* obj)
 {
-  if (!json_is_object(obj)) { exit(1); }
+  if (!json_is_object(obj)) {
+    throw std::runtime_error("Expected json object, got "
+                             + std::string{json_dumps(obj, debug_options)});
+  }
 
   auto* path = json_object_get(obj, spath);
   auto* size = json_object_get(obj, ssize);
@@ -129,7 +156,13 @@ dedup::config::data_file LoadJson<dedup::config::data_file>(json_t* obj)
   if (!path || !json_is_string(path) || !size || !json_is_integer(size)
       || !block_size || !json_is_integer(block_size) || !idx
       || !json_is_integer(idx) || !read_only || !json_is_boolean(read_only)) {
-    exit(1);
+    throw std::runtime_error("Could not parse data_file from "
+                             + std::string{json_dumps(obj, debug_options)});
+  }
+
+  if (json_object_size(obj) > 5) {
+    throw std::runtime_error("Too many keys for data_file: "
+                             + std::string{json_dumps(obj, debug_options)});
   }
 
   dedup::config::data_file df{
@@ -150,6 +183,11 @@ template <typename T> std::vector<T> LoadJsonArray(json_t* arr)
   json_t* val;
   json_array_foreach(arr, idx, val) { vals.push_back(LoadJson<T>(val)); }
 
+  if (json_array_size(arr) != vals.size()) {
+    throw std::runtime_error("Could not convert every json value from: "
+                             + std::string{json_dumps(arr, debug_options)});
+  }
+
   return vals;
 }
 
@@ -157,7 +195,10 @@ dedup::config json_to_conf(json_t* json)
 {
   dedup::config conf;
 
-  if (!json_is_object(json)) { exit(1); }
+  if (!json_is_object(json)) {
+    throw std::runtime_error("Expected json object, got "
+                             + std::string{json_dumps(json, debug_options)});
+  }
 
   json_t* bfiles = json_object_get(json, bstring);
   json_t* pfiles = json_object_get(json, pstring);
@@ -165,24 +206,18 @@ dedup::config json_to_conf(json_t* json)
 
   if (!bfiles || !json_is_array(bfiles) || !pfiles || !json_is_array(pfiles)
       || !dfiles || !json_is_array(dfiles)) {
-    exit(1);
+    throw std::runtime_error("Could not parse config from "
+                             + std::string{json_dumps(json, debug_options)});
+  }
+
+  if (json_object_size(json) > 3) {
+    throw std::runtime_error("Too many keys for config: "
+                             + std::string{json_dumps(json, debug_options)});
   }
 
   conf.bfiles = LoadJsonArray<dedup::config::block_file>(bfiles);
   conf.pfiles = LoadJsonArray<dedup::config::part_file>(pfiles);
   conf.dfiles = LoadJsonArray<dedup::config::data_file>(dfiles);
-
-  json_object_del(json, bstring);
-  json_object_del(json, pstring);
-  json_object_del(json, dstring);
-
-  const char* key;
-  json_t* val;
-  json_object_foreach(json, key, val)
-  {
-    std::cerr << "Unknown key " << key << "(with val " << json_dumps(val, 0)
-              << ")" << std::endl;
-  }
 
   return conf;
 }
@@ -202,11 +237,13 @@ json_t* dump_json<dedup::config::block_file>(
 {
   json_t* obj = json_object();
 
+  if (!obj) { throw std::runtime_error("Could not allocate json object."); }
+
   if (json_object_set_new(obj, spath, json_string(bf.relpath.c_str()))
       || json_object_set_new(obj, sstart, json_integer(bf.Start))
       || json_object_set_new(obj, send, json_integer(bf.End))
       || json_object_set_new(obj, sidx, json_integer(bf.Idx))) {
-    exit(1);
+    throw std::runtime_error("Could not create block file json object.\n");
   }
 
   return obj;
@@ -216,11 +253,13 @@ json_t* dump_json<dedup::config::part_file>(const dedup::config::part_file& pf)
 {
   json_t* obj = json_object();
 
+  if (!obj) { throw std::runtime_error("Could not allocate json object."); }
+
   if (json_object_set_new(obj, spath, json_string(pf.relpath.c_str()))
       || json_object_set_new(obj, sstart, json_integer(pf.Start))
       || json_object_set_new(obj, send, json_integer(pf.End))
       || json_object_set_new(obj, sidx, json_integer(pf.Idx))) {
-    exit(1);
+    throw std::runtime_error("Could not create record file json object.\n");
   }
 
   return obj;
@@ -230,12 +269,14 @@ json_t* dump_json<dedup::config::data_file>(const dedup::config::data_file& df)
 {
   json_t* obj = json_object();
 
+  if (!obj) { throw std::runtime_error("Could not allocate json object."); }
+
   if (json_object_set_new(obj, spath, json_string(df.relpath.c_str()))
       || json_object_set_new(obj, ssize, json_integer(df.Size))
       || json_object_set_new(obj, sbsize, json_integer(df.BlockSize))
       || json_object_set_new(obj, sidx, json_integer(df.Idx))
       || json_object_set_new(obj, srdonly, json_boolean(df.ReadOnly))) {
-    exit(1);
+    throw std::runtime_error("Could not create data file json object.\n");
   }
 
   return obj;
@@ -244,15 +285,30 @@ json_t* dump_json<dedup::config::data_file>(const dedup::config::data_file& df)
 template <typename T> json_t* DumpJsonArray(const std::vector<T>& v)
 {
   json_t* arr = json_array();
+
+  if (!arr) { throw std::runtime_error("Could not allocate json array."); }
+
   for (auto& val : v) {
-    if (json_array_append_new(arr, dump_json<T>(val))) { exit(1); }
+    if (json_array_append_new(arr, dump_json<T>(val))) {
+      throw std::runtime_error("Could not append value to json array.");
+    }
   }
+
+  if (json_array_size(arr) != v.size()) {
+    throw std::runtime_error("Could not append all values to json array (in = "
+                             + std::to_string(v.size()) + ", out = "
+                             + std::to_string(json_array_size(arr)) + ").");
+  }
+
   return arr;
 }
 
 json_t* conf_to_json(const dedup::config& conf)
 {
   json_t* json = json_object();
+
+  if (!json) { throw std::runtime_error("Could not allocate json object."); }
+
   json_t* bfiles = DumpJsonArray(conf.bfiles);
   json_t* pfiles = DumpJsonArray(conf.pfiles);
   json_t* dfiles = DumpJsonArray(conf.dfiles);
@@ -260,8 +316,9 @@ json_t* conf_to_json(const dedup::config& conf)
   if (json_object_set_new(json, bstring, bfiles)
       || json_object_set_new(json, pstring, pfiles)
       || json_object_set_new(json, dstring, dfiles)) {
-    exit(1);
+    throw std::runtime_error("Could not create config json object.");
   }
+
   return json;
 }
 };  // namespace
@@ -280,21 +337,24 @@ int main(int argc, const char* argv[])
 
   CLI11_PARSE(app, argc, argv);
 
-  if (write->parsed()) {
+  try {
     std::vector in = ReadInput();
-    std::string s{in.begin(), in.end()};
-    json_t* root = LoadJson(s.c_str());
-    if (!root) { return 1; }
-    auto conf = json_to_conf(root);
-    std::vector data = dedup::config::serialize(conf);
-    WriteOutput(data);
-  } else if (read->parsed()) {
-    std::vector in = ReadInput();
-    auto conf = dedup::config::deserialize(in.data(), in.size());
-    auto* json = conf_to_json(conf);
-    if (!json) { return 1; }
-    std::cout << json_dumps(json, 0) << std::endl;
-  }
 
-  return 0;
+    if (write->parsed()) {
+      std::string s{in.begin(), in.end()};
+      json_t* root = ParseJson(s.c_str());
+      auto conf = json_to_conf(root);
+      std::vector data = dedup::config::serialize(conf);
+      WriteOutput(data);
+    } else if (read->parsed()) {
+      auto conf = dedup::config::deserialize(in.data(), in.size());
+      auto* json = conf_to_json(conf);
+      std::cout << json_dumps(json, JSON_COMPACT) << std::endl;
+    }
+
+    return 0;
+  } catch (const std::exception& ex) {
+    std::cerr << "Caught unexpected error: " << ex.what() << std::endl;
+    return 1;
+  }
 }
