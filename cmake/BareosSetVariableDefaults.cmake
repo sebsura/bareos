12a44d1197f8218165872300dd278c2eb759dab4
Philipp Storz <philipp.storz@bareos.com>
2024-03-18 15:54:20 +0100

    overwrite default vars for msvc

--- cmake/BareosSetVariableDefaults.cmake
+++ cmake/BareosSetVariableDefaults.cmake
@@ -176,6 +176,77 @@ else() # if(USE_RELATIVE_PATHS)
 
 endif() # if(USE_RELATIVE_PATHS)
 
+if (MSVC)
+
+  set(bindir
+    "C:/Program Files/${CMAKE_PROJECT_NAME}"
+      CACHE STRING "bin directory"
+  )
+  set(sbindir
+    "C:/Program Files/${CMAKE_PROJECT_NAME}"
+      CACHE STRING "sbin directory"
+  )
+  set(libdir
+    "C:/Program Files/${CMAKE_PROJECT_NAME}"
+      CACHE STRING "lib directory"
+  )
+  set(backenddir
+      "${libdir}/backends"
+      CACHE STRING "directory for Bareos backends"
+  )
+  set(plugindir
+      "${libdir}/plugins"
+      CACHE STRING "directory for Bareos plugins"
+  )
+  set(scriptdir
+    "C:/ProgramData/${CMAKE_PROJECT_NAME}/scripts"
+      CACHE STRING "directory for Bareos helper scripts"
+  )
+  set(sysconfdir
+    "C:/ProgramData"
+      CACHE STRING "system configuration directory"
+  )
+  set(SYSCONFDIR "\"${sysconfdir}\"")
+  set(confdir
+      "${sysconfdir}/${CMAKE_PROJECT_NAME}"
+      CACHE STRING "Bareos configuration directory"
+  )
+  set(configtemplatedir
+      "${confdir}"
+      CACHE STRING "directory for Bareos configuration templates (optional)"
+  )
+  set(includedir
+      "${CMAKE_INSTALL_FULL_INCLUDEDIR}/${CMAKE_PROJECT_NAME}"
+      CACHE STRING "include directory"
+  )
+  set(mandir
+      ${CMAKE_INSTALL_FULL_MANDIR}
+      CACHE STRING "man(uals) directory"
+  )
+  set(workingdir
+    "${sysconfdir}/${CMAKE_PROJECT_NAME}/working"
+      CACHE STRING "Bareos working directory"
+  )
+  set(working_dir "${workingdir}")
+  set(archivedir
+    "C:/bareos-storage"
+      CACHE STRING "Bareos archive directory"
+  )
+  set(subsysdir
+      "${workingdir}"
+      CACHE STRING "subsys directory"
+  )
+  set(logdir
+    "C:/ProgramData/${CMAKE_PROJECT_NAME}/logs"
+      CACHE STRING "log directory"
+  )
+  set(datarootdir
+    "C:/ProgramData/${CMAKE_PROJECT_NAME}"
+      CACHE STRING "data root directory"
+  )
+endif() #MSVC
+
+
 set(PYTHON_MODULE_PATH
     "${plugindir}"
     CACHE STRING "Default path for Bareos Python modules"
