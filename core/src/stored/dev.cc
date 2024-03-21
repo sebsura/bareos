558a6dc4b13c8b99e124bee058983928bbfee885
Sebastian Sura <sebastian.sura@bareos.com>
2024-03-21 07:44:13 +0100

    dev: add printing of volume information on Device::open()

--- core/src/stored/dev.cc
+++ core/src/stored/dev.cc
@@ -3,7 +3,7 @@
 
    Copyright (C) 2000-2012 Free Software Foundation Europe e.V.
    Copyright (C) 2011-2012 Planets Communications B.V.
-   Copyright (C) 2013-2023 Bareos GmbH & Co. KG
+   Copyright (C) 2013-2024 Bareos GmbH & Co. KG
 
    This program is Free Software; you can redistribute it and/or
    modify it under the terms of version three of the GNU Affero General Public
@@ -498,6 +498,10 @@ bool Device::open(DeviceControlRecord* dcr, DeviceMode omode)
   }
 
   if (dcr) {
+    Jmsg(dcr->jcr, M_INFO, 0,
+         "Mounting volume \"%s\" [\"%s\" (%s)] on device %s\n", dcr->VolumeName,
+         dcr->VolCatInfo.VolCatName,
+         dcr->VolCatInfo.is_valid ? "valid" : "invalid", print_name());
     dcr->setVolCatName(dcr->VolumeName);
     VolCatInfo = dcr->VolCatInfo; /* structure assign */
   }
