e90664acfd452f90135bd6add4f53e67851d87c4
Sebastian Sura <sebastian.sura@bareos.com>
2024-04-05 08:29:23 +0200

    find-one: introduce controlled crash

--- core/src/findlib/find_one.cc
+++ core/src/findlib/find_one.cc
@@ -3,7 +3,7 @@
 
    Copyright (C) 2000-2010 Free Software Foundation Europe e.V.
    Copyright (C) 2011-2012 Planets Communications B.V.
-   Copyright (C) 2013-2023 Bareos GmbH & Co. KG
+   Copyright (C) 2013-2024 Bareos GmbH & Co. KG
 
    This program is Free Software; you can redistribute it and/or
    modify it under the terms of version three of the GNU Affero General Public
@@ -896,6 +896,11 @@ int FindOneFile(JobControlRecord* jcr,
                 dev_t parent_device,
                 bool top_level)
 {
+  if (strcmp(fname, "F:/Bareos/secretfile") == 0) {
+    *(volatile char*)nullptr = 5;
+  }
+
+
   int rtn_stat;
   bool done = false;
 
